e6b591e31a3e6245a3212ac5b61fdb1de341b623
Yogesh Kumar <balajiyogesh12@gmail.com>
2025-06-24 23:06:26 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    required: true,
+    required: false,
     ref: 'User',
   },
   name: {
